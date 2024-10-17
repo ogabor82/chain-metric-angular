86583451162b6f58f9a2983da22b245ec7124cd5
Ocskó Gábor <ogabor@gmail.com>
2024-10-17 08:51:34 +0200

    new watchlist form

--- src/app/watchlist/new-watchlist/new-watchlist.component.ts
+++ src/app/watchlist/new-watchlist/new-watchlist.component.ts
@@ -1,14 +1,18 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-new-watchlist',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './new-watchlist.component.html',
   styleUrl: './new-watchlist.component.scss',
 })
 export class NewWatchlistComponent {
   @Output() closeDialog = new EventEmitter<void>();
+  watchlistName = '';
+  watchlistDescription = '';
+  watchlistCategories = '';
 
   onClose() {
     this.closeDialog.emit();
